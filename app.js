a0df4aa91294faa51e983a2f958a9eea34d10ed1
markw13a <markw13a@gmail.com>
2018-06-10 14:48:01 +0100

    Switched off homepage redesign. Now just shows original job website

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', jobs);
 app.use('/jobs', jobs);
 
 // catch 404 and forward to error handler
