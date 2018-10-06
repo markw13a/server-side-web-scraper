220d970f3002840017da4738c6af5aa5f2dc1d40
markw13a <markw13a@gmail.com>
2018-10-06 12:21:00 +0100

    Seems to be working on local now

--- vhost_setup.js
+++ vhost_setup.js
@@ -7,17 +7,34 @@
 var express = require('express');
 var path = require('path');
 const vhost = require('vhost');
+
 const markw13aApp = require('./markw13a.co.uk/app.js');
 const unofficialTranslationApp = require('./unofficialtranslations.com/app.js');
 
 var app = express();
+// const markw13aApp = express();
+// const unofficialTranslationApp = express();
+// markw13aApp.get('/', function(req, res) {
+//     res.send('Job Site');
+// });
+// unofficialTranslationApp.get('/', function(req, res) {
+//     res.send('Translation App');
+// });
 
 const createVHost = (domainName, app) => vhost(domainName, app);
 
+// const jobSite = createVHost('markw13a.co.uk', markw13aApp);
 const jobSite = createVHost('markw13a.co.uk', markw13aApp);
+
+// const unofficialTranslationHost = createVHost('unofficialtranslations.com', unofficialTranslationApp);
 const unofficialTranslationHost = createVHost('unofficialtranslations.com', unofficialTranslationApp);
 app
  .use(unofficialTranslationHost)
  .use(jobSite);
+
+// not sure this will ever be invoked?
+app.get('/', function(req, res) {
+    res.send('VHost failure: no app found for given domain');
+});
 
 module.exports = app;
